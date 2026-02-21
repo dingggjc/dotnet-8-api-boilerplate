deed0214b59a94b67e55803e7883aa0427f974b4
dingggjc <charlesaciertojc@gmail.com>
2026-02-21 13:45:58 +0800

    Files upload

---
+++ Flow.Domain/Constant/UserTypes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flow.Domain.Constant
+{
+    public enum UserTypes
+    {
+        SuperAdmin = 1,
+        Owner = 2,
+        Manager = 3,
+        Member = 4,
+        Guest = 5
+
+    }
+}
---
+++ Flow.Domain/User/ApplicationUser.cs
@@ -0,0 +1,37 @@
+﻿
+using Flow.Domain.Constant;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flow.Domain.User
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+
+        public string? MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        public Guid? OrganizationId { get; set; }
+
+        //public virtual Organization? Organization { get; set; }
+
+        public  UserTypes UserTypes { get; set; }
+
+        public DateTimeOffset CreatedAt { set; get; }
+        public string? CreatedById { get; set; }
+        public string? CreatedBy { get; set; }
+        public string? LastModifiedById { get; set; }
+        public string? LastModifiedBy { get; set; }
+        public DateTimeOffset? LastModifiedDate { get; set; }
+        public string? RefreshToken { get; set; }
+        public DateTimeOffset? RefreshTokenExpiryTime { get; set; }
+        public bool IsDeleted { get; set; }
+
+    }
+}
